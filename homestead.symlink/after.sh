a906fc35e782a181361d8979f2b78670e28cb5b1
H-s-O <hso@atelierfolklore.ca>
2017-11-02 10:41:24 -0400

    update

--- homestead.symlink/after.sh
+++ homestead.symlink/after.sh
@@ -4,19 +4,13 @@
 # add any commands you wish to this file and they will
 # be run after the Homestead machine is provisioned.
 
-sudo add-apt-repository ppa:chris-needham/ppa
+# If you have problem with the node.js repository,
+# see https://github.com/nodesource/distributions/issues/324#issuecomment-228028035
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get update
-sudo apt-get -y install chromium-browser xvfb pulseaudio ffmpeg libfdk-aac-dev php-mcrypt audiowaveform
+sudo apt-get -y install chromium-browser xvfb pulseaudio ffmpeg libfdk-aac-dev php-mcrypt audiowaveform php-imagick traceroute nodejs
 
-php /home/vagrant/Code/ffframe.com/artisan migrate
-
-php /home/vagrant/Code/manivelle/backend/artisan migrate
-
-php /home/vagrant/Code/telecommande/artisan migrate
-
-php /home/vagrant/Code/archives.nte.qc.ca/artisan migrate
-
-php /home/vagrant/Code/landandlegends.com/artisan migrate
+#php /home/vagrant/Code/archives.nte.qc.ca/artisan migrate
 
 sudo locale-gen fr_CA
 sudo locale-gen fr_CA.UTF-8
