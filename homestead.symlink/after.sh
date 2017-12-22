02ed765d32a8b113d2a58cd89e5a5a45ee0f7d0c
Hugo St-Onge <hso@atelierfolklore.ca>
2017-12-22 15:42:53 -0500

    fix

--- homestead.symlink/after.sh
+++ homestead.symlink/after.sh
@@ -8,13 +8,11 @@
 # see https://github.com/nodesource/distributions/issues/324#issuecomment-228028035
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get update
-sudo apt-get -y install chromium-browser xvfb pulseaudio ffmpeg libfdk-aac-dev php-mcrypt audiowaveform php-imagick traceroute nodejs
+sudo apt-get -y install chromium-browser xvfb pulseaudio ffmpeg libfdk-aac-dev php-mcrypt php-imagick traceroute
+# sudo apt-get -y install chromium-browser xvfb pulseaudio ffmpeg libfdk-aac-dev php-mcrypt audiowaveform php-imagick traceroute nodejs
 
-<<<<<<< HEAD
 #php /home/vagrant/Code/archives.nte.qc.ca/artisan migrate
 
-=======
->>>>>>> 50618d484dcafd0ed9f565f0f674b65d067cf8c2
 sudo locale-gen fr_CA
 sudo locale-gen fr_CA.UTF-8
 sudo update-locale
